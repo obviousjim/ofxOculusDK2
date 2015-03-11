28a13ee364ddb64acfeeacb1aeed251645083f34
Kyle McDonald <kyle@kylemcdonald.net>
2015-03-11 03:28:12 -0400

    removed example that was said to 'have problems'

--- example-OculusRiftRendering/src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "testApp.h"
-
-//--------------------------------------------------------------
-int main(){
-	// set width, height, mode (OF_WINDOW or OF_FULLSCREEN)
-	ofSetupOpenGL(1024, 768, OF_WINDOW);
-	ofRunApp(new testApp()); // start the app
-}
